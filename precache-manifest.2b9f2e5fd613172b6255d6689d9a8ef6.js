bf0b244346b65db26d651fc6816b6fa74ccee125
Njoki <kinuthiash@gmail.com>
2022-05-30 23:22:09 +0300

    Updates

---
+++ precache-manifest.2b9f2e5fd613172b6255d6689d9a8ef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaf3dbf25c170f9af187",
+    "url": "/weatherapp/static/css/main.62d10f92.chunk.css"
+  },
+  {
+    "revision": "aaf3dbf25c170f9af187",
+    "url": "/weatherapp/static/js/main.aaf3dbf2.chunk.js"
+  },
+  {
+    "revision": "38ecc02d009634cff82d",
+    "url": "/weatherapp/static/js/1.38ecc02d.chunk.js"
+  },
+  {
+    "revision": "3a7099c8f58c3ab6c7e1",
+    "url": "/weatherapp/static/js/2.3a7099c8.chunk.js"
+  },
+  {
+    "revision": "266ef7a5378e88fdf27c",
+    "url": "/weatherapp/static/js/runtime~main.266ef7a5.js"
+  },
+  {
+    "revision": "07c6bd9e1ad53428c37d0dd60d509cad",
+    "url": "/weatherapp/static/media/pietro-de-grandi-T7K4aEPoGGk-unsplash.07c6bd9e.jpg"
+  },
+  {
+    "revision": "e1f582095efc04ec91574e338760ee7c",
+    "url": "/weatherapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36e2d6468e0e0752b66e69e35fcfb358.js"
+  "/weatherapp/precache-manifest.2b9f2e5fd613172b6255d6689d9a8ef6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/weatherapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
